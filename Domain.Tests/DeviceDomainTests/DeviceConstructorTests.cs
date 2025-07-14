84f03c8c8ff80da7a95d2a332744a0850e45a5be
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-07-14 21:45:26 +0100

    Add missing factory test

--- Domain.Tests/DeviceDomainTests/DeviceConstructorTests.cs
+++ Domain.Tests/DeviceDomainTests/DeviceConstructorTests.cs
@@ -5,6 +5,22 @@ namespace Domain.Tests.DeviceDomainTests;
 
 public class ConstructorTests
 {
+
+    [Fact]
+    public void WhenCreatingDeviceWithId_ThenDeviceIsCreated()
+    {
+        //arrange
+        Guid id = Guid.NewGuid(); ;
+        string description = "Work laptop";
+        string brand = "Dell";
+        string model = "Latitude 14";
+        string serialNumber = "1234567890";
+
+        //act & assert
+        new Device(id, description, brand, model, serialNumber);
+
+    }
+
     [Fact]
     public void WhenCreatingDeviceWithValidFields_ThenDeviceIsCreated()
     {
