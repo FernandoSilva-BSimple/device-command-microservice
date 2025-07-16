c5be8120a8e474c358610effd0ae9be2747a3335
Fernando Silva <fernandosilvajfgs@gmail.com>
2025-07-16 20:07:33 +0100

    fix message

--- InterfaceAdapters/Publisher/FakePublisherForTests.cs
+++ InterfaceAdapters/Publisher/FakePublisherForTests.cs
@@ -3,7 +3,7 @@ using Contracts.Messages;
 
 public class FakePublisherForTests : IMessagePublisher
 {
-    public Task PublishDeviceCreatedAsync(DeviceCreatedMessage message)
+    public Task PublishDeviceCreatedAsync(Guid id, string description, string brand, string model, string serialNumber, Guid? assignmentTempId)
     {
         return Task.CompletedTask;
     }
--- InterfaceAdapters/Publisher/MassTransitPublisher.cs
+++ InterfaceAdapters/Publisher/MassTransitPublisher.cs
@@ -14,8 +14,9 @@ public class MassTransitPublisher : IMessagePublisher
         _publishEndpoint = publishEndpoint;
     }
 
-    public async Task PublishDeviceCreatedAsync(DeviceCreatedMessage deviceCreatedMessage)
+    public async Task PublishDeviceCreatedAsync(Guid id, string description, string brand, string model, string serialNumber, Guid? assignmentTempId)
     {
+        var deviceCreatedMessage = new DeviceCreatedMessage(id, description, brand, model, serialNumber, assignmentTempId);
         await _publishEndpoint.Publish(deviceCreatedMessage);
     }
 }
